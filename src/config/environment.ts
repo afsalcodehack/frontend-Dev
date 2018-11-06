36905cd67f7a1dfd87ac58f8195c83654abadb3f
John Vandenberg <jayvdb@gmail.com>
2018-11-06 13:53:10 +0700

    environment.ts: Disable backend again

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,6 +1,6 @@
 export const ENV = {
   LOCATION_STRATEGY: '',  // set to 'path' for /no_hash_pages
   PRODUCTION : false,
-  API_URL    : 'http://localhost:8000/',
+  API_URL    : '',
   STRIPE_KEY : 'pk_test_mtBKhwAg0R3fWQ7NAzulGRuF',
 };
