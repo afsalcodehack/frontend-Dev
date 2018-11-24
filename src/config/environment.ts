ca0013c592d5a10f60369a74df05737936f1d3be
Muhammad Kaisar Arkhan <hi@yukiisbo.red>
2018-11-24 17:10:12 +0700

    environment.ts: Add default local

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,6 +1,6 @@
 export const ENV = {
   LOCATION_STRATEGY: '',  // set to 'path' for /no_hash_pages
   PRODUCTION : false,
-  API_URL    : '',
+  API_URL    : 'http://localhost:8000/',
   STRIPE_KEY : 'pk_test_mtBKhwAg0R3fWQ7NAzulGRuF',
 };
