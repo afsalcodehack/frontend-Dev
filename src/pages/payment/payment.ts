aba44d8aa87ee63625727e31a32016c2661f8224
Your Name <Christian.potts29@gmail.com>
2019-04-10 11:22:44 +0200

    payment: Update ts file

--- src/pages/payment/payment.ts
+++ src/pages/payment/payment.ts
@@ -127,7 +127,7 @@ export class PaymentPage {
 
           const successAlert = await this.alertCtrl.create({
             title: 'Payment Success',
-            subTitle: 'You have purchased {{product}}',
+            subTitle: 'You have purchased a photo from {{title}}',
             buttons: ['OK'],
           }, { subTitle: { product: this.title }});
 
